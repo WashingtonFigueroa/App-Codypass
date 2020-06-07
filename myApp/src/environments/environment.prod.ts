98dbc4c6792c19a4c0d22af594ca1b4c48e93173
Washington Figueroa <w.figo.1991@gmail.com>
2020-06-06 19:39:24 -0500

    conex

--- myApp/src/environments/environment.prod.ts
+++ myApp/src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  servidor: 'http://localhost:8000/api/'
+    servidor: 'https://codypass.com/ScanQR/servidor/public/api/',
+ // servidor: 'http://localhost:8000/api/'
 };
